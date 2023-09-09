a953d47a9b3cca6d0b229bea469240e2ad99174a
aelberethen <fabio.marfia@polimi.it>
2023-09-09 18:18:41 +0200

    Aggiunta traduzione italiana

--- provaProgettoCondiviso/src/provaProgettoCondiviso/Main.java
+++ provaProgettoCondiviso/src/provaProgettoCondiviso/Main.java
@@ -4,5 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World!");
+		System.out.println("In italiano vuol dire:");
+		System.out.println("Buongiorno mondo");
 	}
 }
